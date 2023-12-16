efb0080173d4656cbd4b36249347772d59bf9e79
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-16 20:04:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 
 const express = require("express");
 const app = express();
+const path = require("path")
 const port = 3000;
+
+app.use(express.static(path.join(__dirname+"/public")))
 
 app.get("/getText", (req, res) => {
   res.send("Hello World!");
