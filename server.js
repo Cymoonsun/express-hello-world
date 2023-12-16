621d07f4de40d7733ad33f461bec2e09fdfae569
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-16 23:18:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const Redis = require("redis")
 const port = 3000;
 
-const redisClient = Redis.createClient({url:"redis-16837.c135.eu-central-1-1.ec2.cloud.redislabs.com:16837"})
+const redisClient = Redis.createClient({url:"redis://default:ZKCaiysAgbLmDjWeHlUnOQVj6DoZu6JF@redis-16837.c135.eu-central-1-1.ec2.cloud.redislabs.com:16837"})
 const expire = 3600
 
 app.use(express.static(path.join(__dirname+"/public")))
@@ -23,6 +23,11 @@ app.get("/getUser", (req, res)=>{
 })
 
 app.listen(port, async () => {
-  await redisClient.connect()
+  try{
+    await redisClient.connect()
+  }
+  catch(err){
+    console.error(err)
+  }
   console.log(`Example app listening on port ${port}`);
 });
