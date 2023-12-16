45eb1e13bb69a0ef6ecb2a0584be1b717486a30b
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-16 19:50:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.get('/getText', (req, res) => {
-  res.send("Here is text");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+app.get('/getText', (req, res) => {
+  res.send("Here is text");
 });
