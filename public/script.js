d90d0b875246d0978553cbecb9e6a079875ea44e
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-16 22:49:28 +0100

    Redis stuff maybe

--- public/script.js
+++ public/script.js
@@ -2,8 +2,19 @@
 async function getRandomNumber() {
     try {
       const response = await fetch('https://jffun.cyclic.app/getText');
-      console.log(response)
+      const resText = await response.text()
+      console.log(resText)
     } catch (error) {
       console.error('Error fetching random number:', error);
     }
   }
+document.addEventListener("DOMContentLoaded", async ()=>{
+    try {
+        const response = await fetch('https://jffun.cyclic.app/getUser');
+        console.log(response)
+        const resText = await response.json()
+        console.log(resText)
+      } catch (error) {
+        console.error('Error fetching random number:', error);
+      }
+})
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-// Example of web server written with Express
-
 const express = require("express");
 const app = express();
 const path = require("path")
+const Redis = require("redis")
 const port = 3000;
+
+const redisClient = await Redis.createClient().connect()
+const expire = 3600
 
 app.use(express.static(path.join(__dirname+"/public")))
 
 app.get("/getText", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get("/getUser", (req, res)=>{
+ redisClient.get("myUser", (err, user)=>{
+      if (err) console.error(err)
+      if(user != null){
+        res.json(JSON.parse(user))
+      }
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
