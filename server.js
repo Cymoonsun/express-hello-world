476dd90e33ca076162348cf298fb703d01d4ae47
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-16 19:15:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const app = require('./app');
-
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
+// Define the getRandomNumber endpoint
 app.get('/getRandomNumber', (req, res) => {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   res.json({ randomNumber });
