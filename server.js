433baa766227e319aaabeacd18ded9b829495efd
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-16 18:49:02 +0100

    First service

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get('/getRandomNumber', (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1; // Change the range as needed
+  res.json({ randomNumber });
+});
