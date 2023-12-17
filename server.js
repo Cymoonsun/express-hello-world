4d83ee1b2eb1d4d0ea41fc3fe0b1b034ef3a7975
Cymoonsun <153231599+Cymoonsun@users.noreply.github.com>
2023-12-17 18:28:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const Redis = require("redis")
 const port = 3000;
 
-const redisClient = await Redis.createClient({
+const redisClient = Redis.createClient({
     password: 'q1Piyo5hnsiAdLJugFN2aUMTrhwtvRJu',
     socket: {
         host: 'redis-16643.c250.eu-central-1-1.ec2.cloud.redislabs.com',
