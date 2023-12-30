1a83ccd21a2b74d486e3855d0184e4c30e98a5f9
Cymoonsun <simone.civica00@gmail.com>
2023-12-30 17:44:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('trust proxy', 1);
+app.set('trust proxy', true);
 
 const upload = multer({
   limits: {
